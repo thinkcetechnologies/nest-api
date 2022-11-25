2ac7999f860346e849e5756435aba7c7152fd8ea
thinkce <thinkce123@gmail.com>
2022-11-25 12:10:11 +0000

    start implementing employee functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { CompanyModule } from './company/company.module';
 import { BranchModule } from './branch/branch.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), UserModule, PrismaModule, CompanyModule, BranchModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), UserModule, PrismaModule, CompanyModule, BranchModule, EmployeeModule],
 })
 export class AppModule {}
 
